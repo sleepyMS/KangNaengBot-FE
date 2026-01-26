828747a2b9d6c01577a50e4a19898eccfd9f9ad7
sleepyMS <minseok011017@gmail.com>
2026-01-26 22:12:31 +0900

    fix: 빌드 에러 수정

--- src/components/settings/tabs/AccountTab.tsx
+++ src/components/settings/tabs/AccountTab.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { LogOut, Mail, User, UserX } from "lucide-react";
